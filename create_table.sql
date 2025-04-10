8c9d590c7c71991b07512eeb00338d416d70be97
Daniel A. Esteban <daniel@datak.co>
2025-04-10 13:09:21 -0500

    Improvements for deployment with env vars

---
+++ create_table.sql
@@ -0,0 +1,6 @@
+
+CREATE TABLE students (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    age INTEGER NOT NULL
+);
--- main.py
+++ main.py
@@ -1,8 +1,22 @@
 from fastapi import FastAPI
+from database import get_db_connection
+from dotenv import load_dotenv
+import os 
 
+load_dotenv()
 app = FastAPI()
 
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World from Google Cloud Engine"}
+    return {"Hello": os.getenv("HOLA_MUNDO")}
+
+@app.get("/students")
+def read_students():
+    db = get_db_connection()
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM students")
+    students = cursor.fetchall()
+    cursor.close()
+    db.close()
+    return {"students": students}
