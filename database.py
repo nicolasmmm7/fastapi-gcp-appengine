1a52aa7a736df1dd1f7209910c4c7ad1b8da0b68
Nicolás Meneses <felipemenesespeinado5@gmail.com>
2025-10-20 17:46:10 -0500

    avance

--- database.py
+++ database.py
@@ -3,10 +3,10 @@ import psycopg2
 from psycopg2.extras import RealDictCursor
 
 # Configuración de Cloud SQL
-DB_HOST = os.getenv("DB_HOST", "")
-DB_NAME = os.getenv("DB_NAME", "")
-DB_USER = os.getenv("DB_USER", "")
-DB_PASSWORD = os.getenv("DB_PASSWORD", "")
+DB_HOST = os.getenv("DB_HOST", "34.66.222.114")
+DB_NAME = os.getenv("DB_NAME", "postgres")
+DB_USER = os.getenv("DB_USER", "postgres")
+DB_PASSWORD = os.getenv("DB_PASSWORD", "Nube1234@")
 
 def get_db_connection():
     return psycopg2.connect(
