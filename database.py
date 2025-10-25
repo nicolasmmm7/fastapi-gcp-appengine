0782a30fe585519395ca1616519d564456a988ec
Nicolás Meneses <felipemenesespeinado5@gmail.com>
2025-10-25 18:33:42 -0500

    actividad nube

--- database.py
+++ database.py
@@ -7,6 +7,8 @@ DB_HOST = os.getenv("DB_HOST", "34.66.222.114")
 DB_NAME = os.getenv("DB_NAME", "postgres")
 DB_USER = os.getenv("DB_USER", "postgres")
 DB_PASSWORD = os.getenv("DB_PASSWORD", "Nube1234@")
+DB_PORT = os.getenv("DB_PORT", "5432")
+
 
 def get_db_connection():
     return psycopg2.connect(
@@ -14,5 +16,6 @@ def get_db_connection():
         database=DB_NAME,
         user=DB_USER,
         password=DB_PASSWORD,
+        port=DB_PORT,
         cursor_factory=RealDictCursor
     )
--- main.py
+++ main.py
@@ -1,15 +1,21 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from database import get_db_connection
 from dotenv import load_dotenv
-import os 
+from pydantic import BaseModel
+import os
 
 load_dotenv()
 app = FastAPI()
+
+class Student(BaseModel):
+    name: str
+    age: int
 
 
 @app.get("/")
 def read_root():
     return {"Hello": os.getenv("HOLA_MUNDO")}
+
 
 @app.get("/students")
 def read_students():
@@ -20,3 +26,33 @@ def read_students():
     cursor.close()
     db.close()
     return {"students": students}
+
+
+@app.post("/students")
+def create_student(student: Student):
+    db = get_db_connection()
+    cursor = db.cursor()
+    try:
+        cursor.execute(
+            """
+            INSERT INTO students (name, age)
+            VALUES (%s, %s)
+            RETURNING id, name, age;
+            """,
+            (student.name, student.age)
+        )
+        new_student = cursor.fetchone()
+        db.commit()
+        return {
+            "message": "Student added successfully",
+            "student": new_student
+        }
+    except Exception as e:
+        db.rollback()
+        raise HTTPException(
+            status_code=500,
+            detail=f"Error inserting student: {str(e)}"
+        )
+    finally:
+        cursor.close()
+        db.close()
